6009ee1473b488aec137429e0cd41648b8e8b157
n288TJYRX <n288TJYRX@tutanota.com>
2020-01-30 09:15:45 +0000

    changed handler

--- library/conductor-library/src/main/java/uk/gov/gchq/gaffer/workflow/handler/RunWorkflowHandler.java
+++ library/conductor-library/src/main/java/uk/gov/gchq/gaffer/workflow/handler/RunWorkflowHandler.java
@@ -11,6 +11,7 @@ import uk.gov.gchq.gaffer.store.Store;
 import uk.gov.gchq.gaffer.store.operation.handler.OperationHandler;
 import uk.gov.gchq.gaffer.workflow.RunWorkflow;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 import static uk.gov.gchq.gaffer.workflow.util.ConductorEndpoint.*;
@@ -37,11 +38,13 @@ public class RunWorkflowHandler implements OperationHandler<RunWorkflow> {
         System.out.println("Workflow Id: " + workflowId);
 
         // Get the output of the workflow using the workflow Id
-        HashMap workflowOutput = null;
-        HashMap<String, Object> responseMap = new HashMap<>();
+        Object workflowOutput = null;
+        HashMap responseMap = new HashMap<>();
         String workflowStatus = "";
         long timeStart = System.currentTimeMillis();
         while (!workflowStatus.equals("COMPLETED") && !workflowStatus.equals("FAILED") && !workflowStatus.equals("TIMED OUT")) {
+            responseMap = new HashMap<>();
+
             // If the workflow hasn't completed after a long while stop checking
             if (System.currentTimeMillis() > timeStart + WORKFLOW_TIMEOUT) {
                 throw new OperationException("Workflow timed out");
@@ -56,8 +59,10 @@ public class RunWorkflowHandler implements OperationHandler<RunWorkflow> {
 
             // Get the response and deserialise it
             String response = http("GET",BASE_URI+WORKFLOW_START_ENDPOINT+"/"+workflowId,null);
+            System.out.println("Workflow response: " + response);
             try {
                 responseMap = JSONSerialiser.deserialise(response,HashMap.class);
+//                System.out.println("Workflow response map: " + responseMap.toString());
                 workflowStatus = (String) responseMap.get("status");
                 System.out.println("Workflow status: " + workflowStatus);
             } catch (SerialisationException e) {
@@ -66,11 +71,11 @@ public class RunWorkflowHandler implements OperationHandler<RunWorkflow> {
         }
 
         // Get the workflow output
-        workflowOutput = (HashMap) responseMap.get("output");
-        System.out.println("WorkflowOutput is: " + workflowOutput);
-        Object result = workflowOutput.get("results");
-        System.out.println("Workflow result is: " + result);
-
-        return workflowOutput;
+        workflowOutput = responseMap.get("output");
+        System.out.println("WorkflowOutput is: " + workflowOutput.toString());
+//        HashMap results = (HashMap) workflowOutput.get("output");
+//        System.out.println("Results are: " + results.toString());
+        String results = null;
+        return Arrays.asList(results);
     }
 }
